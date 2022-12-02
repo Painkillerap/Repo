5adf728bf62eddc5a430e0442fc94aa4fb56e2e1
Painkillerap <painkiller.ap@gmail.com>
2022-12-02 15:25:47 +0300

    Lekc2

---
+++ ex010MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 12, 44, 86, 77, 99, 68, 43, 64, 86 };
+
+int n = array.Length;
+int find = 86;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index=index+1
+    index++;
+}
