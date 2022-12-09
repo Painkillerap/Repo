754e5159b4f64f851a33c623df81d152a383e1c9
Painkillerap <painkiller.ap@gmail.com>
2022-12-09 17:08:35 +0300

    Lekz3

---
+++ ex012/Program.cs
@@ -0,0 +1,61 @@
+﻿//2
+/*
+void Method(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method(msg: "Текст", count: 4);
+Method(count: 4, msg: "Текст2");
+*/
+//3
+/*
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+*/
+//4
+/*
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
+*/
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "asdf");
+//Console.WriteLine(res);
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} ={i * j}");
+    }
+    System.Console.WriteLine();
+}
